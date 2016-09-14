679069c1f0c4cdb311a87ba50a7fe13f0e89bcf1
nlkassad <nlkassad@hotmail.com>
2016-09-14 16:10:32 -0400

    Correct Sign-up Loop Error

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -24,12 +24,12 @@ const signInSuccess = (data) => {
   toggleLoggedInOut();
 };
 
-const signUpSuccess = (data) => {
-  app.user = data.user;
+const signUpSuccess = () => {
+//  app.user = data.user;
   document.getElementById("sign-up").reset();
-  setNavMessage("You have an account! Now you may need to sign in...");
-  $('[data-id="games-played"]').empty();
-  // removed authentication ui toggle 
+  setNavMessage("Cool yo, now sign in...");
+//  $('[data-id="games-played"]').empty();
+  // removed authentication ui toggle
 };
 
 const changePasswordSuccess = () => {
